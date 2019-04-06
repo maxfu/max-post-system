a50329df4d8dfd9a829a21fb82f46f551e19168f
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-04-06 11:04:40 +0000

    Redirect

--- templates/single-survey.php
+++ templates/single-survey.php
@@ -1,41 +1,8 @@
-<?php get_header(); ?>
 
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-	<style type="text/css">
-	.cid-qUC8WLfjwn {
-		<?php if ( has_post_thumbnail() ) { ?>
-		background-image: url("<?php the_post_thumbnail_url('full'); ?>");
-		<?php } else { ?>
-		background-image: url("<?php echo get_template_directory_uri(); ?>/assets/images/jumbotron.jpg");
-		<?php } ?>
-	}
-	</style>
-
-	<main role="main" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-		<section class="mbr-section content5 cid-qUC8WLfjwn mbr-parallax-background" id="content5-1y">
-			<div class="mbr-overlay" style="opacity: 0.4; background-color: rgb(35, 35, 35);"></div>
-			<div class="container">
-				<div class="media-container-row">
-					<div class="title col-12">
-						<h2 class="align-center mbr-bold mbr-white mbr-fonts-style display-1"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="features11 cid-qSShVnZyJK" id="content4-p">
-			<div class="container">
-				<?php $destination_link = get_post_meta( get_the_ID(), 'destination_link', true ); ?>
-          <div class="media-container-row">
-  			<div class="mbr-text col-9 col-md-9 mbr-fonts-style display-7">
-  			  <p>
-                <?php echo __( 'Survey Link: ', 'max-post' ) . $destination_link; ?><br>
-              </p>
-  			</div>
-  		  </div>
-		</div>
-		</section>
-	</main>
+	<?php $destination_link = get_post_meta( get_the_ID(), 'destination_link', true ); ?>
+	<?php header('Location: '. $destination_link ); ?>
+	<?php exit(); ?>
 <?php endwhile; ?>
 <?php else: ?>
 	<main role="main">
@@ -50,5 +17,3 @@
 		</section>
 	</main>
 <?php endif; ?>
-
-<?php get_footer(); ?>
